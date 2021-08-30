e6f02da092f8698067175924f65f7764dc002db2
JonathanT10 <jonathan_terrell@yahoo.com>
2021-08-30 13:31:08 -0500

    Connected to MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const connectDB = require('./startup/db');
+const app = express();
+const cors = require('cors');
+const product = require('./routes/product');
 
 
-connectDB
+connectDB();
 
 
 app.use(express.json());
 app.use(cors());
+app.use('/api/product/', product);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
